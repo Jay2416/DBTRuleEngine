17ae0df370d84fa4dec356ef181275fce2f21b1f
Jay Patel <98154473+Jay2416@users.noreply.github.com>
2026-03-24 14:48:32 +0530

    Delete dbt/netflix directory

--- dbt/netflix/models/staging/src_genome_tags.sql
+++
@@ -1,8 +0,0 @@
-with raw_genome_tag as (
-    select * from {{ source('raw_data', 'genome_tags') }}
-)
-
-select 
-    tagid as tag_id,
-    tag
-from raw_genome_tag
--- dbt/netflix/models/staging/src_movies.sql
+++
@@ -1,9 +0,0 @@
-with raw_movies as (
-    select * from {{ source('raw_data', 'movies') }}
-)
-
-select 
-    movieid as movie_id,
-    title,
-    genres
-from raw_movies
